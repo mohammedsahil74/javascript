f8490e25ef361cff74c42927985064374feaa3b4
mohammedsahil <sahilmohammed7474@gmail.com>
2025-04-11 21:06:05 +0530

    added contacts function

--- day3.js
+++ day3.js
@@ -5,14 +5,20 @@ function printInfo(){
   console.log('-------------------------');
   console.log('1. Add Contact.');
   console.log('2. Delete Contact.');
-  console.log('3. Search Contacts.');
-  console.log('4. View Contact.');
+  console.log('3. view Contacts.');
+  console.log('4. search Contact.');
   console.log('5. Exit!');
 }
 printInfo();
 
 function addContact(){
-
+    const name = prompt('enter your name:')
+    const email = prompt('enter your email:')
+    const Contact ={
+      name :name,
+      email:email
+    }
+    contacts.push(Contact)
 }
 function deleteContact(){
 
@@ -21,18 +27,20 @@ function searchContact(){
 
 }
 function viewContact(Contacts){
-
+  console.log(contacts)
 }
 
-
-const number = number(prompt('enter your operation(1-5)'));
+const contacts = [];
 let keepGoing = true;
 
-while(true){
 
+while(true){
+  
+  const number = prompt('enter your operation(1-5)');
 switch (number){
   case '1':
    addContact()
+   
     break;
 
   case '2':
@@ -40,11 +48,11 @@ switch (number){
     break;
 
   case '3':
-    searchContact()
+    viewContact(contacts)
     break;
 
   case '4':
-    viewContact()
+   searchContact()
     break;
 
   case '5':
@@ -52,6 +60,6 @@ switch (number){
     break;
 
   default:
-    console.log('unknow')
+    console.log('unknown')
     break;
 }};
